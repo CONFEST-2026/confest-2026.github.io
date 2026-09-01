9395799d91289f123eadaa92cb6105211fb9e5c0
David Purser <d.purser@liverpool.ac.uk>
2026-09-01 11:25:41 +0100

    make sure tomorrow it will default to wednesdsay

--- _includes/js/program.js
+++ _includes/js/program.js
@@ -9,21 +9,26 @@ window.conference.program = (() => {
         if ($("#day-list")) {
             // Switch to day if page load with hash
             const hash = window.location.hash;
-            if (hash) {
-                $('#day-list a[href="' + hash + '"]').tab("show");
+            const hashTab = hash ? $('#day-list a[href="' + hash + '"]') : $();
+            if (hashTab.length) {
+                hashTab.tab("show");
             }
 
             // Switch to day if today
             else {
-                const now = new Date();
-                const tsNow = Math.floor(now.getTime() / 1000);
+                const tsNow = Math.floor(Date.now() / 1000);
 
-                $('a[data-toggle="tab"]').each(function () {
-                    const tsMidnight = new Date($(this).data("ts") * 1000);
+                $('#day-list a[data-toggle="tab"]').each(function () {
+                    const tsMidnight = parseInt($(this).data("ts"), 10);
 
-                    if (tsMidnight <= tsNow < tsMidnight + 24 * 60 * 60) {
+                    if (isNaN(tsMidnight)) {
+                        return true;
+                    }
+
+                    if (tsNow >= tsMidnight && tsNow < tsMidnight + 24 * 60 * 60) {
                         $(this).tab("show");
                         updateHash(this.hash);
+                        return false;
                     }
                 });
             }
